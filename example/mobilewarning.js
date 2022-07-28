36b47792cb3af406c5e6ca51e0c166c606169a53
Manolol <manolol@mailfence.com>
2022-07-28 18:11:01 +0200

    why tf did I type game instead of website here!?

--- example/mobilewarning.js
+++ example/mobilewarning.js
@@ -3,7 +3,7 @@ const ua = navigator.userAgent;
 console.log("Checking device type...")
 
 if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
-    alert("It seems like you are using a mobile device. Please note, that this game doesn't work really good on mobile devices, but you can still try it. If you are not using a mobile device, just ignore this warning.")
+    alert("It seems like you are using a mobile device. Please note, that this website doesn't work really good on mobile devices, but you can still try it. If you are not using a mobile device, just ignore this warning.")
     console.warn("Device type checked. Result:Mobile Please note, that this website doesn't work really good on mobile devices, but you can still try it. If you are not using a mobile device, just ignore this warning.")
 }else{
     console.log("Device type checked. Result:Desktop")
